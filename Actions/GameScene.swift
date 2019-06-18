bb6769105c76ef95e95fb02d7eb8224cc20a0052
MacStudent <macstudent@130-IMAC-14.local>
2019-06-18 17:57:24 -0400

    changing scene

--- Actions/GameScene.swift
+++ Actions/GameScene.swift
@@ -11,8 +11,8 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+ 
+   
     
     override func didMove(to view: SKView) {
         
